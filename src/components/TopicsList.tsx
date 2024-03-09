0fee8aaabd15862a102b2ca96d98bece087b3d3a
RusdyZain <ryanfajri28@gmail.com>
2024-03-09 08:29:02 +0800

    Backend Done

--- src/components/TopicsList.tsx
+++ src/components/TopicsList.tsx
@@ -3,59 +3,48 @@ import Remove from "@/components/button/Remove";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 
-const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/topics";
-
 interface Topic {
   _id: string;
   title: string;
   description: string;
 }
 
-export default function TopicsList() {
-  const [topics, setTopics] = useState<Topic[]>([]);
+const TopicsList = () => {
+  const [topics, setTopics] = useState<Topic[]>();
 
   useEffect(() => {
-    const fetchTopics = async () => {
-      try {
-        const response = await fetch(API_URL);
-        if (!response.ok) {
-          throw new Error("Failed to fetch topics");
-        }
-        const data = await response.json();
-        setTopics(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchTopics();
+    fetch("http://localhost:3000/api/topics")
+      .then((res) => res.json())
+      .then((data) => {
+        const topicsArray = data.topics;
+        setTopics(topicsArray);
+      })
+      .catch((error) => console.error("Error fetching topics:", error));
   }, []);
+
+  if (!topics) return <p>Loading Data ...</p>;
 
   return (
     <>
-      {topics.length === 0 && <p>Loading topics...</p>}
-      {topics.length > 0 && (
-        <>
-          {topics.map((topic) => (
-            <div
-              key={topic._id}
-              className="p-4 border border-state-300 my-3 flex justify-between gap-5 items-center"
-            >
-              <div>
-                <h2 className="font-bold text-2xl">{topic.title}</h2>
-                <div>{topic.description}</div>
-              </div>
-              <div className="flex gap-2">
-                <Remove />
-                <Link href={`/editTopic/${topic._id}`}>
-                  <HiPencilAlt size={24} />
-                </Link>
-              </div>
-            </div>
-          ))}
-        </>
-      )}
+      {topics.map((topic) => (
+        <div
+          key={topic._id} // Gunakan key yang unik untuk setiap topik
+          className="p-4 border border-state-300 my-3 flex justify-between gap-5 items-center"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{topic.title}</h2>
+            <div>{topic.description}</div>
+          </div>
+          <div className="flex gap-2">
+            <Remove />
+            <Link href={`/editTopic/${topic._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
     </>
   );
-}
+};
+
+export default TopicsList;
