cd5a3baf101fed18fa809d3c73e347ff7e3debf5
RusdyZain <ryanfajri28@gmail.com>
2024-03-09 23:15:19 +0800

    Project DONE

--- src/components/EditTopicForm.tsx
+++ src/components/EditTopicForm.tsx
@@ -1,22 +1,74 @@
 import Layout from "@/pages/layout/Layout";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useState, useEffect } from "react";
 
-export default function EditTopicForm() {
+export default function EditTopicForm({
+  id,
+  title,
+  description,
+}: {
+  id: string;
+  title: string;
+  description: string;
+}) {
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
+  const router = useRouter();
+
+  useEffect(() => {
+    setNewTitle(title);
+    setNewDescription(description);
+  }, [title, description]);
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          newTitle,
+          newDescription,
+        }),
+      });
+
+      if (!res.ok) {
+        const data = await res.json();
+        console.error(data);
+        return;
+      }
+
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Layout>
-      <form action="" className="flex flex-col gap-3">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3">
         <input
+          onChange={(e) => setNewTitle(e.target.value)}
+          value={newTitle}
           className="border border-slate-500 px-8 py-2"
           type="text"
           placeholder="Topic Title"
         />
         <input
+          onChange={(e) => setNewDescription(e.target.value)}
+          value={newDescription}
           className="border border-slate-500 px-8 py-2"
           type="text"
           placeholder="Topic Description"
         />
 
-        <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+        <button
+          className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
+          type="submit"
+        >
           Edit Topic
         </button>
       </form>
--- src/pages/editTopic/[id]/index.tsx
+++ src/pages/editTopic/[id]/index.tsx
@@ -1,6 +1,39 @@
+import { useRouter } from "next/router";
 import EditTopicForm from "@/components/EditTopicForm";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function EditTopic() {
-  return <EditTopicForm />;
+interface Topic {
+  _id: string;
+  title: string;
+  description: string;
+}
+
+export default function EditTopic(): JSX.Element {
+  const router = useRouter();
+  const { id } = router.query;
+  const [topics, setTopics] = useState<Topic[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/topics")
+      .then((res) => res.json())
+      .then((data) => {
+        const topicsArray = data.topics;
+        setTopics(topicsArray);
+      })
+      .catch((error) => console.error("Error fetching topics:", error));
+  }, []);
+
+  const topic = topics.find((topic) => topic._id === id);
+
+  if (!topic) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <EditTopicForm
+      id={id as string}
+      title={topic.title}
+      description={topic.description}
+    />
+  );
 }
